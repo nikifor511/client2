ded3cc0c689f76985f32de8b159b76886e7421dd
nikifor511 <deadnikifor@gmail.com>
2018-05-17 12:04:41 +0300

    nikifor511

--- app/src/main/java/garant/n/client/MainActivity.java
+++ app/src/main/java/garant/n/client/MainActivity.java
@@ -2,6 +2,8 @@ package garant.n.client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void on_connect_button_click(View view)
+    {
+        Log.e("button:", "connect");
+
+    }
+
+    protected void on_send_button_click(View view)
+    {
+
     }
 }
